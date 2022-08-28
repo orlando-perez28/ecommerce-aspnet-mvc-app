168f634e01f23230ef6a238b2fc2497c10b06b4e
orlando perez <orlando_perez@outlook.com>
2022-08-27 21:44:15 -0500

    Data migration added, database seed with data

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,10 +1,12 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("EcommerceCnn")));
 
 var app = builder.Build();
 
@@ -26,5 +28,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+AppDbInitializer.Seed(app);
 
 app.Run();
