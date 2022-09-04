ae1d64798263753958f6ceccb613891cdd1a3350
orlando perez <orlando_perez@outlook.com>
2022-09-03 19:27:53 -0500

    Generics added

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace eTickets.Data.Base;
+
+public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+{
+    private readonly AppDbContext _appDbContext;
+
+    public EntityBaseRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _appDbContext.Set<T>().AddAsync(entity);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+
+    public async Task DeleteAsync(int id)
+    {
+        var result = await _appDbContext.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
+        EntityEntry entityEntry = _appDbContext.Entry<T>(result);
+        entityEntry.State = EntityState.Deleted;
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()=> await _appDbContext.Set<T>().ToListAsync();
+    
+    public async Task<T> GetByIdAsync(int id)=> await _appDbContext.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
+
+
+    public async Task UpdateAsync(int id, T entity)
+    {
+        EntityEntry entityEntry =_appDbContext.Entry<T>(entity);
+        entityEntry.State = EntityState.Modified;     
+        await _appDbContext.SaveChangesAsync();
+    }
+}
